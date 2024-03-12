a919d370cc94ad126e37881c5c6e0bba51926f72
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2024-03-12 09:32:14 +0100

    Main Structure including Placeholders done Navigation structure done Header Banner Design finished

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//////////////////////////////----------------------------Hide Navbar when scroll----------------------------//////////////////////////////
+
+let prevScrollpos = window.scrollY;
+window.onscroll = function () {
+    let currentScrollPos = window.scrollY;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("header").style.top = "0";
+    } else {
+        document.getElementById("header").style.top = "-400px";
+    }
+    prevScrollpos = currentScrollPos;
+}
