ff1b6dddf5133100561242e81a99ef5445bdbd27
LittlePumpkin87 <littlepumpkin.jr@googlemail.com>
2024-04-20 10:12:34 +0200

    solved scemantic errors

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-//////////////////////////////----------------------------Hide Hamburger and Navbar when scroll----------------------------//////////////////////////////
-/* 
-let prevScrollpos = window.scrollY;
-window.onscroll = function () {
-    let currentScrollPos = window.scrollY;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("header").style.top = "0";
-    } else {
-        document.getElementById("header").style.top = "-20rem";
-    }
-    prevScrollpos = currentScrollPos;
-}; */
 //////////////////////////////----------------------------Hamburger Navigation Show/hide----------------------------//////////////////////////////
 
 $("#HamburgerToggle").on("click", function() {
